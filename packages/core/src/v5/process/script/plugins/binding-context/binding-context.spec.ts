cb867aa6904d1455a1cb877999a98617a9d3b470
pikax <carlos@hypermob.co.uk>
2025-01-18 23:17:14 +0000

    add binding context return for setup

---
+++ packages/core/src/v5/process/script/plugins/binding-context/binding-context.spec.ts
@@ -0,0 +1,95 @@
+import { MagicString } from "@vue/compiler-sfc";
+import { parser } from "../../../../parser";
+import { ParsedBlockScript } from "../../../../parser/types";
+import { processScript } from "../../script";
+
+import { BindingContextPlugin } from "./index.js";
+import { MacrosPlugin } from "../macros";
+
+describe("process script plugin binding context", () => {
+  function _parse(
+    content: string,
+    wrapper: string | boolean = false,
+    lang = "js",
+    attributes = "",
+
+    pre = "",
+    post = ""
+  ) {
+    const prepend = `${pre}<script ${
+      wrapper === false ? "setup" : ""
+    } lang="${lang}" ${attributes}>`;
+    const source = `${prepend}${content}</script>${post}`;
+    const parsed = parser(source);
+
+    const s = new MagicString(source);
+
+    const scriptBlock = parsed.blocks.find(
+      (x) => x.type === "script"
+    ) as ParsedBlockScript;
+
+    const r = processScript(
+      scriptBlock.result.items,
+      [
+        BindingContextPlugin,
+        MacrosPlugin,
+        // // clean template tag
+        // {
+        //   post: (s) => {
+        //     s.update(pre.length, pre.length + prepend.length, "");
+        //     s.update(source.length - "</script>".length - pos, source.length, "");
+        //   },
+        // },
+      ],
+      {
+        s,
+        filename: "test.vue",
+        blocks: parsed.blocks,
+        isSetup: wrapper === false,
+        block: scriptBlock,
+        generic: parsed.generic,
+      }
+    );
+
+    return r;
+  }
+
+  //   describe("ts", () => {});
+
+  //   it.only("test", () => {
+  //     const { s } = _parse(`export default { setup(){ defineExpose({ a: 0 }); }}`, 'ddd');
+  //     expect(s.original).toBe(s.toString());
+  //   });
+  
+  describe('ts', ()=> {
+    describe('setup', ()=> {
+        function parse(content: string, attributes = '') {
+            return _parse(content, false, 'ts', attributes);
+        }
+        it('work', ()=> {
+            const {result} = parse('let a = 0')
+            expect(result).toMatchInlineSnapshot(`"export function ___VERTER___BindingContext(){ let a = 0}"`)
+        })
+
+        it('attributes', ()=> {
+            const {result} = parse('let a = 0;', 'attributes="HTMLAttributes"')
+            expect(result).toMatchInlineSnapshot(`
+              "export function ___VERTER___BindingContext(){ let a = 0;} /**
+               * ___VERTER___ATTRIBUTES
+               */type ___VERTER___attributes=HTMLAttributes;"
+            `)
+        })
+
+        it('generic', ()=> {
+            const {result} = parse('let a = 0;', 'generic="T"')
+            expect(result).toMatchInlineSnapshot(`"export function ___VERTER___BindingContext<T>(){let a = 0;}"`)
+        })
+
+        it.only('defineProps', ()=> {
+            const {result} = parse('const props = defineProps({ a: String })')
+            expect(result).toMatchInlineSnapshot(`"export function ___VERTER___BindingContext(){ const props=___VERTER___Props = defineProps({ a: String });return {props: typeof props,___VERTER___Props: typeof ___VERTER___Props}}"`)
+        })
+    })
+
+  })
+});
--- packages/core/src/v5/process/script/plugins/binding-context/binding-context.ts
+++ packages/core/src/v5/process/script/plugins/binding-context/binding-context.ts
@@ -1,3 +1,5 @@
+import { BlockPlugin } from "../../../template/plugins";
+import { ProcessItemType } from "../../../types";
 import { definePlugin } from "../../types";
 
 export const BindingContextPlugin = definePlugin({
@@ -10,23 +12,118 @@ export const BindingContextPlugin = definePlugin({
 
   post(s, ctx) {
     // this should update the <script > ... with the function bindings
-    // then return it 
+    // then return it
     // and then have the fullBinding context
+
+    const block = ctx.block.block.block;
+    const tag = ctx.block.block.tag;
+
+    const isTs = block.lang?.startsWith("ts");
+    const isSetup = !!block.attrs.setup;
+    const isAsync = ctx.isAsync;
+    const generic = ctx.generic;
+
+    const bindingContext = ctx.prefix("BindingContext");
+
+    // remove close tag
+    s.update(tag.pos.close.start, tag.pos.close.end, "");
+
+    let attrStart: number | undefined = undefined;
+    let attrEnd: number | undefined = undefined;
+
+    // handle attributes
+    if (typeof block.attrs.attributes === "string") {
+      const attributesContentStart =
+        tag.content.indexOf(block.attrs.attributes) + tag.pos.content.start;
+      const attributesStart = attributesContentStart - 'attributes="'.length;
+
+      attrStart = attributesStart - 1;
+      attrEnd = attributesContentStart + block.attrs.attributes.length;
+
+      const ATTRIBUTES = ctx.prefix("ATTRIBUTES");
+      const prefix = ctx.prefix("");
+      const preAttributes = `/**\n * ${ATTRIBUTES}\n */type `;
+
+      s.prependLeft(attributesStart, prefix);
+      s.prependLeft(attributesStart, preAttributes);
+      s.prependLeft(attrEnd, ";");
+
+      // remove delimiter
+      s.remove(attributesContentStart - 1, attributesContentStart);
+      s.remove(
+        attributesContentStart + block.attrs.attributes.length,
+        attributesContentStart + block.attrs.attributes.length + 1
+      );
+
+      // move attribute to the end
+      s.move(attrStart, attrEnd, tag.pos.close.end);
+    }
+
+    if (isSetup) {
+      const preGeneric = `export ${
+        isAsync ? "async " : ""
+      }function ${bindingContext}`;
+      const postGeneric = `(){`;
+      s.update(tag.pos.open.start, tag.pos.content.start, preGeneric);
+
+      if (generic) {
+        const genericStart =
+          tag.content.indexOf(generic.source) + tag.pos.content.start;
+        const genericEnd = genericStart + generic.source.length;
+
+        // remove before generic
+        s.remove(tag.pos.content.start, genericStart);
+        // remove after generic
+        s.remove(genericEnd, tag.pos.open.end);
+
+        // update generic
+        s.prependLeft(genericStart, "<");
+        s.prependLeft(genericEnd, ">" + postGeneric);
+      } else {
+        s.prependLeft(tag.pos.content.start, postGeneric);
+        // update tag start to attribute start
+        s.remove(tag.pos.content.start + 1, attrStart ?? tag.pos.open.end);
+
+        if (attrStart) {
+          s.remove(attrEnd!, tag.pos.open.end);
+        }
+      }
+
+      // handle return
+
+      const extraBindings = ctx.items.filter(
+        (x) => x.type === ProcessItemType.Binding
+      );
+      const returnItems = Array.from(this.items)
+        .concat(extraBindings.map((x) => x.name))
+        .map((x) => `${x}: typeof ${x}`)
+        .join(",");
+
+      s.prependLeft(tag.pos.close.end, `;return {${returnItems}}`);
+
+      // close
+      s.appendLeft(tag.pos.close.end, "}");
+    } else {
+    }
   },
 
   // add known bindings
   transformDeclaration(item, s, ctx) {
-    if (
-      item.parent.type === "VariableDeclarator" &&
-      item.parent.init?.type === "CallExpression"
-    ) {
-      if (item.parent.init.callee.type === "Identifier") {
-        const name = item.parent.init.callee.name;
-        this.items.add(name);
-      }
+    if (item.name) {
+      this.items.add(item.name);
     }
+    // if (item.parent.type === "VariableDeclarator") {
+    //   if (item.parent.init?.type === "CallExpression") {
+    //     if (item.parent.init.callee.type === "Identifier") {
+    //       const name = item.parent.init.callee.name;
+    //       this.items.add(name);
+    //     }
+    //   } else if (item.parent.id?.type === "Identifier") {
+    //     this.items.add(item.parent.id.name);
+    //   }
+    // }
   },
-  transformFunctionCall(item, s, ctx) {
-    this.items.add(item.name);
-  },
+  //   transformFunctionCall(item, s, ctx) {
+  //     this.items.add(item.name);
+  //   },
 });
