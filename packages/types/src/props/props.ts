916a211bd01e0c02b50e980620d6400cb5dcf306
pikax <carlos@hypermob.co.uk>
2025-11-17 16:39:04 +0000

    props

--- packages/types/src/props/props.ts
+++ packages/types/src/props/props.ts
@@ -1,10 +1,17 @@
-
+import { MacroReturn, MacroReturnObject, MacroReturnType } from "../setup";
 /**
  * These are the props used in the template or public API of the component.
  * They have optional props made optional.
  */
 export type MakePublicProps<T extends Record<PropertyKey, any>> =
-  T extends import("vue").DefineProps<infer P, any>
+  T extends MacroReturn<infer Value extends Record<PropertyKey, any>, any>
+    ? T extends MacroReturnType<
+        Value,
+        infer Type extends Record<PropertyKey, any>
+      >
+      ? MakePublicProps<Type>
+      : MakePublicProps<Value>
+    : T extends import("vue").DefineProps<infer P, any>
     ? T extends import("vue").DefineProps<P, infer K extends keyof P>
       ? P & Partial<Pick<P, K>>
       : T
@@ -13,7 +20,7 @@ export type MakePublicProps<T extends Record<PropertyKey, any>> =
 /**
  * These are the internal props of the component.
  * They have all props required.
- * 
+ *
  * They are accessed inside the component implementation.
  */
 export type MakeInternalProps<T extends Record<PropertyKey, any>> =
@@ -22,3 +29,25 @@ export type MakeInternalProps<T extends Record<PropertyKey, any>> =
       ? P & Required<Pick<P, K>>
       : T
     : T;
+
+// --- Test ---
+
+import { createMacroReturn, ExtractMacroReturn, ExtractProps } from "../setup";
+
+const setupReturn = createMacroReturn({
+  props: {
+    value: {
+      foo: "" as string,
+      bar: 0 as number,
+    },
+    type: {} as {
+      foo?: string;
+      bar: number;
+    },
+  },
+});
+
+type Extracted = ExtractProps<ExtractMacroReturn<typeof setupReturn>>;
+type Props = MakePublicProps<Extracted>;
+declare const foo: Props;
+foo;
