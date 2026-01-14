b273ddd721f2d174af1708d473fe1f3edd62fb28
pikax <carlos@hypermob.co.uk>
2026-01-14 09:56:35 +0000

    more direc

--- packages/core/src/v5/process/template/plugins/directives/directives.ts
+++ packages/core/src/v5/process/template/plugins/directives/directives.ts
@@ -4,13 +4,15 @@ import {
   ElementTypes,
   Namespaces,
   NodeTypes,
+  SimpleExpressionNode,
 } from "@vue/compiler-core";
-import { TemplateDirective } from "../../../../parser";
+import { ParseTemplateContext, TemplateDirective } from "../../../../parser";
 import { declareTemplatePlugin, TemplateContext } from "../../template";
 import { ProcessItemType } from "../../../types";
 import { createHelperImport } from "../../../utils";
 import { capitalize, isBuiltInDirective } from "@vue/shared";
 import { AvailableExports } from "@verter/types/string";
+import { BindingPlugin } from "../binding";
 
 function pushWarning(
   node: DirectiveNode,
@@ -82,11 +84,234 @@ export const DirectiveRunnerPlugin = declareTemplatePlugin({
     this.directivesByElement.clear();
   },
 
-  transformDirective(directive, _s, ctx) {
-    validateBuiltInDirective(directive.name, directive.node, ctx);
-    const list = this.directivesByElement.get(directive.element) ?? [];
-    list.push(directive);
-    this.directivesByElement.set(directive.element, list);
+  transformDirective(item, s, ctx) {
+    // handle type checking for directives
+    validateBuiltInDirective(item.name, item.node, ctx);
+    const list = this.directivesByElement.get(item.element) ?? [];
+    list.push(item);
+    this.directivesByElement.set(item.element, list);
+    // /handle type checking for directives
+
+    const element = item.element;
+    const node = item.node;
+
+    switch (item.name) {
+      case "model": {
+        const clonedS = s.clone();
+        // because the binding is not yet transformed
+        // we need to clone the source and transform the binding
+        item.exp?.forEach((x) => {
+          BindingPlugin.transformBinding!(x, clonedS, ctx);
+        });
+        item.arg?.forEach((x) => {
+          BindingPlugin.transformBinding!(x, clonedS, ctx);
+        });
+
+        const fallbakName =
+          element.tagType === ElementTypes.ELEMENT ? "value" : "modelValue";
+
+        let bindingTo =
+          element.tagType === ElementTypes.ELEMENT ? "input" : "modelValue";
+
+        let isDynamic = false;
+
+        if (node.arg) {
+          const arg = node.arg as SimpleExpressionNode;
+
+          if (!node.arg.ast && arg.isStatic) {
+            bindingTo = arg.content;
+            s.overwrite(node.loc.start.offset, arg.loc.end.offset, bindingTo);
+          } else {
+            isDynamic = true;
+
+            // remove v-model
+            s.overwrite(node.loc.start.offset, arg.loc.start.offset, "");
+            // replace = with :
+            if (s.original[arg.loc.end.offset] === "=") {
+              s.overwrite(arg.loc.end.offset, arg.loc.end.offset + 1, ":");
+            }
+            s.prependLeft(node.loc.start.offset, "{...{");
+          }
+        } else {
+          s.overwrite(
+            node.loc.start.offset,
+            node.loc.start.offset + "v-model".length,
+            fallbakName
+          );
+        }
+
+        if (node.exp) {
+          // update delimiters
+          if (isDynamic) {
+            s.remove(node.exp.loc.start.offset - 1, node.exp.loc.start.offset);
+          } else {
+            s.overwrite(
+              node.exp.loc.start.offset - 1,
+              node.exp.loc.start.offset,
+              "{"
+            );
+          }
+
+          // this will be updated in the next iteration
+          // s.overwrite(node.exp.loc.end.offset, node.exp.loc.end.offset + 1, "}");
+
+          const exp = clonedS.slice(
+            node.exp.loc.start.offset,
+            node.exp.loc.end.offset
+          );
+
+          if (isDynamic) {
+            bindingTo = clonedS
+              .slice(node.arg!.loc.start.offset, node.arg!.loc.end.offset)
+              .toString()
+              .slice(1, -1);
+          }
+
+          const eventName =
+            element.tagType === ElementTypes.ELEMENT
+              ? "onInput"
+              : isDynamic
+              ? "onUpdate"
+              : `onUpdate:${bindingTo}`;
+
+          const valueAccessor =
+            element.tagType === ElementTypes.ELEMENT
+              ? "$event.target.value"
+              : "$event";
+          const pre = isDynamic
+            ? `,[\`${eventName}:\${${bindingTo}}\`]:`
+            : `} ${eventName}={`;
+
+          const post = isDynamic ? "}}" : "}";
+
+          s.overwrite(
+            node.exp.loc.end.offset,
+            node.exp.loc.end.offset + 1,
+            `${pre}($event)=>(${exp.toString()}=${valueAccessor})${post}`
+          );
+        } else {
+          // shouldn't be here
+          // todo add a warning
+
+          ctx.items.push({
+            type: ProcessItemType.Warning,
+            message: "NO_EXPRESSION_VMODEL",
+            start: node.loc.start.offset,
+            end: node.loc.end.offset,
+            node,
+          });
+        }
+
+        break;
+      }
+      case "is": {
+        if (item.element.tag === "component") {
+          return;
+        }
+      }
+      default: {
+        const directiveAccessor = ctx.retrieveAccessor("directiveAccessor");
+        const instancePropertySymbol = ctx.retrieveAccessor(
+          "instancePropertySymbol"
+        );
+        const slotInstance = ctx.retrieveAccessor("slotInstance");
+        const instanceToDirectiveFn = ctx.retrieveAccessor(
+          "instanceToDirectiveFn"
+        );
+        const instanceToDirectiveVar = ctx.retrieveAccessor(
+          "instanceToDirectiveVar"
+        );
+        const directiveName = ctx.retrieveAccessor("directiveName");
+
+        const context = item.context as ParseTemplateContext;
+        if (ctx.doNarrow && context.conditions.length > 0) {
+          ctx.doNarrow(
+            {
+              index: node.loc.start.offset,
+              conditions: context.conditions,
+              inBlock: true,
+              type: "prepend",
+              direction: "left",
+            },
+            s
+          );
+        }
+
+        const declaration =
+          `const ${instanceToDirectiveVar}=${instanceToDirectiveFn}(${slotInstance});` +
+          `const ${directiveName}=${instanceToDirectiveVar}(`;
+
+        s.prependLeft(
+          node.loc.start.offset,
+          `{...{[${instancePropertySymbol}]:(${slotInstance})=>{`
+        );
+
+        s.prependRight(node.loc.start.offset, `${directiveAccessor}.`);
+        s.prependRight(node.loc.start.offset, declaration);
+
+        // replace ={\w} with {\w} uppercase letter
+        s.overwrite(
+          node.loc.start.offset + 1,
+          node.loc.start.offset + 3,
+          item.name[0].toUpperCase()
+        );
+
+        s.prependLeft(node.loc.start.offset + 2 + item.name.length, ");");
+
+        if (node.arg) {
+          const arg = node.arg as SimpleExpressionNode;
+          // replace ':' with '='
+          s.overwrite(arg.loc.start.offset - 1, arg.loc.start.offset, "=");
+
+          s.prependRight(arg.loc.start.offset - 1, `${directiveName}.arg`);
+          s.prependLeft(arg.loc.end.offset, ";");
+
+          if (arg.isStatic) {
+            // add quotes
+            s.prependLeft(arg.loc.start.offset, '"');
+            s.prependLeft(arg.loc.end.offset, '"');
+          }
+        }
+
+        if (node.modifiers.length > 0) {
+          const start = node.modifiers[0].loc.start.offset;
+          const end = node.modifiers[node.modifiers.length - 1].loc.end.offset;
+
+          s.overwrite(start - 1, start, `${directiveName}.modifiers=[`);
+          s.prependLeft(end, "];");
+
+          for (let i = 0; i < node.modifiers.length; i++) {
+            const modifier = node.modifiers[i];
+            if (i > 0) {
+              s.overwrite(
+                modifier.loc.start.offset - 1,
+                modifier.loc.start.offset,
+                ","
+              );
+            }
+            s.prependRight(modifier.loc.start.offset, '"');
+            s.prependLeft(modifier.loc.end.offset, '"');
+          }
+        }
+
+        if (node.exp) {
+          // replace ="
+          s.overwrite(
+            node.exp.loc.start.offset - 2,
+            node.exp.loc.start.offset,
+            `${directiveName}.value=`
+          );
+
+          s.overwrite(
+            node.exp.loc.end.offset,
+            node.exp.loc.end.offset + 1,
+            ";"
+          );
+        }
+
+        s.prependRight(node.loc.end.offset, "}}}");
+      }
+    }
   },
 
   transformProp(prop, _s, ctx) {
